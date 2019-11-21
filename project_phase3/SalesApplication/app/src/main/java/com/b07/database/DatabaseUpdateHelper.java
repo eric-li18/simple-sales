c140311e7ee5a3d85326d0892c51a1bfb1c5a23b
liubryan@UTORONTO.CA <liubryan@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-21 22:31:06 +0000

    Bryan Liu: DatabaseInsertHelper, used Math.exactInt; DatabaseUpdateHelper, refactored for android driver

--- project_phase3/SalesApplication/app/src/main/java/com/b07/database/DatabaseUpdateHelper.java
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/database/DatabaseUpdateHelper.java
@@ -1,180 +1,141 @@
 package com.b07.database;
 
-import com.b07.database.DatabaseUpdater;
+import android.content.Context;
 import com.b07.validation.Validator;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class DatabaseUpdateHelper extends DatabaseUpdater {
+public class DatabaseUpdateHelper {
 
 
-  public static boolean updateRoleName(String name, int id) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateRoleName(String name, int id, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateRoleName(name) && Validator.validateRoleId(id)) {
-      complete = DatabaseUpdater.updateRoleName(name, id, connection);
+      complete = dbA.updateRoleName(name, id);
       if (!complete) {
         System.out.println("Could not update roleName in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateUserName(String name, int userId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateUserName(String name, int userId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateUserId(userId) && Validator.validateUserName(name)) {
-      complete = DatabaseUpdater.updateUserName(name, userId, connection);
+      complete = dbA.updateUserName(name, userId);
       if (!complete) {
         System.out.println("Could not update userName in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateUserAge(int age, int userId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateUserAge(int age, int userId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateUserId(userId) && Validator.validateAge(age)) {
-      complete = DatabaseUpdater.updateUserAge(age, userId, connection);
+      complete = dbA.updateUserAge(age, userId);
       if (!complete) {
         System.out.println("Could not update user age in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateUserAddress(String address, int userId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateUserAddress(String address, int userId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateUserId(userId) && Validator.validateAddress(address)) {
-      complete = DatabaseUpdater.updateUserAddress(address, userId, connection);
+      complete = dbA.updateUserAddress(address, userId);
       if (!complete) {
         System.out.println("Could not update user address in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateUserRole(int roleId, int userId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateUserRole(int roleId, int userId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateUserId(userId) && Validator.validateRoleId(roleId)) {
-      complete = DatabaseUpdater.updateUserRole(roleId, userId, connection);
+      complete = dbA.updateUserRole(roleId, userId);
       if (!complete) {
         System.out.println("Could not update user role in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateItemName(String name, int itemId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateItemName(String name, int itemId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateItemId(itemId) && Validator.validateItemName(name)) {
-      complete = DatabaseUpdater.updateItemName(name, itemId, connection);
+      complete = dbA.updateItemName(name, itemId);
       if (!complete) {
         System.out.println("Could not update item name in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateItemPrice(BigDecimal price, int itemId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateItemPrice(BigDecimal price, int itemId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
 
     if (Validator.validateItemId(itemId) && Validator.validatePrice(price)) {
-      complete = DatabaseUpdater.updateItemPrice(price, itemId, connection);
+      complete = dbA.updateItemPrice(price, itemId);
       if (!complete) {
         System.out.println("Could not update item price in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
-      }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
-    }
+    dbA.close();
     return complete;
   }
 
-  public static boolean updateInventoryQuantity(int quantity, int itemId) {
-    Connection connection = DatabaseDriverHelper.connectOrCreateDataBase();
-
+  public static boolean updateInventoryQuantity(int quantity, int itemId, Context context) {
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
     boolean complete = false;
     quantity = quantity + DatabaseSelectHelper.getInventoryQuantity(itemId);
 
     if (Validator.validateRestockQuantity(quantity) && Validator.validateItemId(itemId)) {
-      complete = DatabaseUpdater.updateInventoryQuantity(quantity, itemId, connection);
+      complete = dbA.updateInventoryQuantity(quantity, itemId);
       if (!complete) {
         System.out.println("Could not update inventory quantity in the database.");
       }
     }
-    try {
-      if (connection != null) {
-        connection.close();
+    dbA.close();
+    return complete;
+  }
+
+  public static boolean updateAccountStatus(int accountId, boolean active, Context context){
+    DatabaseDriverAndroid dbA = new DatabaseDriverAndroid(context);
+    boolean complete = false;
+
+    /**
+     * Needs sanitization
+     */
+    if(true){
+      complete = dbA.updateAccountStatus(accountId, active);
+      if (!complete){
+        System.out.println("Could not update inventory quantity in the database.");
       }
-    } catch (SQLException ex) {
-      System.out.println("Connection not closed.");
     }
+    dbA.close();
     return complete;
   }
 }
