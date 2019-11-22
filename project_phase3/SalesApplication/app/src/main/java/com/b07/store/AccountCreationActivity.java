80bb0269413d46054a9953e79bd9cf242845e3c5
liubryan@UTORONTO.CA <liubryan@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-22 19:31:15 +0000

    Bryan Liu: MainActivity: decides whether to init with sign up or login; added AccountCreationActivity and StoreAuthenticationActivity

---
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/AccountCreationActivity.java
@@ -0,0 +1,17 @@
+package com.b07.store;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.b07.R;
+
+
+public class AccountCreationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.sign_up);
+
+
+  }
+}
