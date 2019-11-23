2be17569e3b57333e00405e3501cce9333393139
liubryan@UTORONTO.CA <liubryan@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-23 18:14:19 +0000

    Bryan Liu: added welcome.xml, added SignUpButtonController to comply with SRP; AccountCreationActivity, refactored to work with the the new button controller

--- project_phase3/SalesApplication/app/src/main/java/com/b07/store/AccountCreationActivity.java
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/AccountCreationActivity.java
@@ -1,67 +1,29 @@
 package com.b07.store;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.b07.R;
-import com.b07.validation.Validator;
 
 
 public class AccountCreationActivity extends AppCompatActivity {
-
-  TextView sign_up;
-  TextView error;
-  EditText name;
-  EditText age;
-  EditText address;
-  EditText password;
-  EditText password2;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.sign_up);
-    sign_up = (TextView) findViewById(R.id.sign_up);
-    error = (TextView) findViewById(R.id.error);
-    name = (EditText) findViewById(R.id.name);
-    age = (EditText) findViewById(R.id.age);
-    address = (EditText) findViewById(R.id.address);
-    password = (EditText) findViewById(R.id.password);
-    password2 = (EditText) findViewById(R.id.reenter_password);
 
-    sign_up.setText("Sign up as Admin");
+    Intent intent = getIntent();
+    String role = intent.getStringExtra("role");
+
+    TextView sign_up = findViewById(R.id.sign_up);
+    EditText age = findViewById(R.id.age);
+    Button signUpButton = findViewById(R.id.sign_up_button);
+
+    sign_up.setText(intent.getStringExtra("sign_up_display"));
+    age.setTransformationMethod(null);
+    signUpButton.setOnClickListener(new SignUpButtonController(this, role));
   }
-
-  String TAG = "AccountCreation";
-
-
-  public void signUp(View view) {
-    boolean accountError = false;
-
-    Log.e(TAG, "im here");
-    if (!Validator.validateUserName(name.getText().toString().trim())) {
-      error.setText(R.string.name_eror);
-      accountError = true;
-    } else if (Validator.validateEmpty(age.getText().toString()) || !Validator.validateAge(Integer.parseInt(age.getText().toString().trim()))) {
-      error.setText(R.string.age_error);
-      accountError = true;
-    } else if (!Validator.validateAddress(address.getText().toString().trim())) {
-      error.setText(R.string.address_error);
-      accountError = true;
-    } else if (!Validator.validatePassword(password.getText().toString())) {
-      error.setText(R.string.password_error);
-      accountError = true;
-    } else if (!password.getText().toString().equals(password2.getText().toString())) {
-      error.setText(R.string.password_mismatch);
-      accountError = true;
-    }
-
-    if (!accountError) {
-      error.setText("yay u created an account!");
-    }
-  }
-
 }
---
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/SignUpButtonController.java
@@ -0,0 +1,76 @@
+package com.b07.store;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.b07.R;
+import com.b07.database.DatabaseInsertHelper;
+import com.b07.validation.Validator;
+
+public class SignUpButtonController implements View.OnClickListener {
+
+  private Context appContext;
+  private String role;
+
+  public SignUpButtonController(Context context, String role) {
+    appContext = context;
+    this.role = role;
+  }
+
+  @Override
+  public void onClick(View view) {
+    boolean accountError = false;
+
+    EditText name = ((AccountCreationActivity) appContext).findViewById(R.id.name);
+    EditText age = ((AccountCreationActivity) appContext).findViewById(R.id.age);
+    EditText address = ((AccountCreationActivity) appContext).findViewById(R.id.address);
+    EditText password = ((AccountCreationActivity) appContext)
+        .findViewById(R.id.password);
+    EditText password2 = ((AccountCreationActivity) appContext)
+        .findViewById(R.id.reenter_password);
+
+    TextView error = ((AccountCreationActivity)appContext).findViewById(R.id.error);
+
+    String parseName = name.getText().toString().trim();
+    Integer parseAge = -1;
+    if(!Validator.validateEmpty(age.getText().toString())){
+          parseAge = Integer.parseInt(age.getText().toString());
+    }
+    String parseAddress = address.getText().toString().trim();
+    String parsePassword = password.getText().toString();
+    String parsePassword2 = password2.getText().toString();
+
+    if (!Validator.validateUserName(parseName)) {
+      error.setText(R.string.name_eror);
+      accountError = true;
+    } else if (!Validator.validateAge(parseAge)) {
+      error.setText(R.string.age_error);
+      accountError = true;
+    } else if (!Validator.validateAddress(parseAddress)) {
+      error.setText(R.string.address_error);
+      accountError = true;
+    } else if (!Validator.validatePassword(parsePassword)) {
+      error.setText(R.string.password_error);
+      accountError = true;
+    } else if (!parsePassword.equals(parsePassword2)) {
+      error.setText(R.string.password_mismatch);
+      accountError = true;
+    }
+
+    if (!accountError) {
+      int userId = DatabaseInsertHelper.insertNewUser(parseName,
+          Integer.parseInt(age.getText().toString().trim()), parseAddress,
+          parsePassword, appContext);
+      int roleId = DatabaseInsertHelper.insertRole(role, appContext);
+      DatabaseInsertHelper.insertUserRole(userId, roleId, appContext);
+
+      Intent intent = new Intent(appContext, WelcomeActivity.class);
+      intent.putExtra("name", parseName);
+      intent.putExtra("role", role);
+      intent.putExtra("userId", String.valueOf(userId));
+      appContext.startActivity(intent);
+    }
+  }
+}
