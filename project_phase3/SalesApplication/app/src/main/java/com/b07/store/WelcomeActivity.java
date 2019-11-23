245079353e0632f52aabbe8fc83a93fdc27d7033
liubryan@UTORONTO.CA <liubryan@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-23 19:10:53 +0000

    Bryan Liu: WelcomeContinueButton: made it start MainActivity again; MainActivity: starts up on sign up or login depending on what is needed in the database

--- project_phase3/SalesApplication/app/src/main/java/com/b07/store/WelcomeActivity.java
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/WelcomeActivity.java
@@ -2,23 +2,25 @@ package com.b07.store;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.b07.R;
 import com.b07.users.Roles;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.welcome);
 
-    TextView name = (TextView) findViewById(R.id.welcome_name);
-    TextView role = (TextView) findViewById(R.id.welcome_role);
-    TextView userId = (TextView) findViewById(R.id.welcome_user_id);
-    TextView anSwitch = (TextView) findViewById(R.id.an_switch);
+    TextView name = findViewById(R.id.welcome_name);
+    TextView role = findViewById(R.id.welcome_role);
+    TextView userId = findViewById(R.id.welcome_user_id);
+    TextView anSwitch = findViewById(R.id.an_switch);
+
+    Button continueButton = findViewById(R.id.welcome_continue_button);
+    continueButton.setOnClickListener(new WelcomeContinueButtonController(this));
 
     Intent intent = getIntent();
 
@@ -30,10 +32,5 @@ public class WelcomeActivity extends AppCompatActivity {
     name.setText(intent.getStringExtra("name"));
     userId.setText(intent.getStringExtra("userId"));
     role.setText(role_name);
-  }
-
-
-  public void continue_button() {
-    finish();
   }
 }
---
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/WelcomeContinueButtonController.java
@@ -0,0 +1,20 @@
+package com.b07.store;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import com.b07.MainActivity;
+
+public class WelcomeContinueButtonController implements View.OnClickListener {
+  private Context appContext;
+
+  public WelcomeContinueButtonController(Context context){
+    appContext = context;
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent intent = new Intent(appContext, MainActivity.class);
+    appContext.startActivity(intent);
+  }
+}
