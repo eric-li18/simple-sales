4cb7f12ec05e2488b17a054bd672547d8970a225
liubryan@UTORONTO.CA <liubryan@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-23 23:45:12 +0000

    Bryan Liu:AddCustomerButtonController, you can now sign up a customer user

---
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/employee/AddCustomerButtonController.java
@@ -0,0 +1,26 @@
+package com.b07.store.employee;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import com.b07.R;
+import com.b07.store.signup.AccountCreationActivity;
+import com.b07.users.Roles;
+
+public class AddCustomerButtonController implements View.OnClickListener {
+
+  private Context appContext;
+
+  public AddCustomerButtonController(Context context) {
+    appContext = context;
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent intent = new Intent(appContext, AccountCreationActivity.class);
+    intent.putExtra("sign_up_display",
+        appContext.getResources().getString(R.string.customer_sign_up));
+    intent.putExtra("role", Roles.CUSTOMER.name());
+    appContext.startActivity(intent);
+  }
+}
