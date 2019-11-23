2dad43a7762057df458851fc85f12c3c25740b4c
eric-li18 <eric.kh.li0821@gmail.com>
2019-11-23 23:09:45 +0000

    Eric Li: CustomerUIActivity.java, added logout functionality with button and added text fill for greeting; LogoutBUttonController.java, added support for logout button from customeruiactivity ADDED

---
+++ SalesApplication/app/src/main/java/com/b07/store/CustomerUIActivity.java
@@ -0,0 +1,30 @@
+package com.b07.store;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.b07.R;
+
+/**
+ * @author Eric
+ */
+public class CustomerUIActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.customer);
+
+    TextView greeting = findViewById(R.id.greeting);
+    ImageView logout = findViewById(R.id.customer_logout);
+
+    Intent intent = getIntent();
+
+    greeting.setText(intent.getStringExtra("name"));
+    logout.setOnClickListener(new LogoutButtonController(this));
+
+  }
+}
