4879d4b4bc5c6822869e603c51563b6740bc5fc2
eric-li18 <eric.kh.li0821@gmail.com>
2019-11-24 15:59:27 +0000

    Eric Li: CustomerUIActivity.java, added functionality to print out items on the layout

--- SalesApplication/app/src/main/java/com/b07/store/customer/CustomerUIActivity.java
+++ SalesApplication/app/src/main/java/com/b07/store/customer/CustomerUIActivity.java
@@ -7,12 +7,44 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.b07.R;
+import com.b07.database.DatabaseInsertHelper;
+import com.b07.database.DatabaseSelectHelper;
+import com.b07.inventory.Item;
+import com.b07.inventory.ItemTypes;
 import com.b07.store.LogoutButtonController;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author Eric
  */
 public class CustomerUIActivity extends AppCompatActivity {
+
+  private void renderShop() {
+    int count = 1;
+    for (ItemTypes item : ItemTypes.values()) {
+      String itemIdName = "customer_itemname" + count;
+      String priceIdName = "customer_itemprice" + count;
+      int itemId = getResources().getIdentifier(itemIdName, "id", getPackageName());
+      int priceId = getResources().getIdentifier(priceIdName, "id", getPackageName());
+
+      TextView itemTextView = findViewById(itemId);
+      TextView priceTextView = findViewById(priceId);
+      List<Item> allItems = DatabaseSelectHelper.getAllItems(this);
+      BigDecimal price = BigDecimal.ZERO;
+      for (Item i : allItems) {
+        if (i.getName().equals(item.name())) {
+          price = i.getPrice();
+          break;
+        }
+      }
+      String itemName = (item.name().substring(0, 1).toUpperCase() + item.name().substring(1)
+          .toLowerCase()).replace("_", " ");
+      itemTextView.setText(itemName);
+      priceTextView.setText(price.toString());
+      count++;
+    }
+  }
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,12 +53,16 @@ public class CustomerUIActivity extends AppCompatActivity {
 
     TextView greeting = findViewById(R.id.greeting);
     ImageView logout = findViewById(R.id.customer_logout);
-
     Intent intent = getIntent();
-
-    greeting.setText(intent.getStringExtra("name"));
+    String greetingText = "Hi " + intent.getStringExtra("name") + ",";
+    greeting.setText(greetingText);
     logout.setOnClickListener(new LogoutButtonController(this));
-
+    float count = 1.00f;
+    for (ItemTypes i : ItemTypes.values()) {
+      DatabaseInsertHelper.insertItem(i.name(), new BigDecimal(count), this);
+      count += 1.00f;
+    }
+    renderShop();
   }
 
   @Override
