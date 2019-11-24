07941bbec40c7b24658d4968f50efa8388fa5c5c
liubryann <liubryan.developer@gmail.com>
2019-11-24 07:28:10 +0000

    Bryan Liu: all UI can log out now

--- SalesApplication/app/src/main/java/com/b07/store/admin/AdminUIActivity.java
+++ SalesApplication/app/src/main/java/com/b07/store/admin/AdminUIActivity.java
@@ -1,13 +1,18 @@
 package com.b07.store.admin;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.b07.R;
+import com.b07.store.LogoutButtonController;
 
 public class AdminUIActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.admin);
+
+    ImageView logoutButton = findViewById(R.id.admin_logout);
+    logoutButton.setOnClickListener(new LogoutButtonController(this));
   }
 }
