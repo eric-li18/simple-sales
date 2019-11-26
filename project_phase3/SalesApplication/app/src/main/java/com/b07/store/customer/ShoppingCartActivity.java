15e0e45470cacdb6da947a5be6572a9ffe1ba4a7
lieric12@UTORONTO.CA <lieric12@UTORONTO.CA@29e768a8-45fc-4eb4-bf6f-e0b42a9c937c>
2019-11-26 23:18:51 +0000

    Eric Li: additional file mishap

--- project_phase3/SalesApplication/app/src/main/java/com/b07/store/customer/ShoppingCartActivity.java
+++ project_phase3/SalesApplication/app/src/main/java/com/b07/store/customer/ShoppingCartActivity.java
@@ -3,11 +3,14 @@ package com.b07.store.customer;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.b07.R;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.shopping_cart);
+
   }
 }
