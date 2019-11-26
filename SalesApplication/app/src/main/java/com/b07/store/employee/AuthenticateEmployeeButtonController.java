9833e04115e9811de49adbbdddecf7034582705c
liubryann <liubryan.developer@gmail.com>
2019-11-26 06:47:02 +0000

    Bryan Liu: AuthenticateEmployeeButtonController, authenticates a new employee

---
+++ SalesApplication/app/src/main/java/com/b07/store/employee/AuthenticateEmployeeButtonController.java
@@ -0,0 +1,20 @@
+package com.b07.store.employee;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import com.b07.store.login.StoreAuthenticationActivity;
+
+public class AuthenticateEmployeeButtonController implements View.OnClickListener{
+  private Context appContext;
+
+  public AuthenticateEmployeeButtonController(Context context){
+    appContext = context;
+  }
+
+  @Override
+  public void onClick(View v) {
+    Intent intent = new Intent(appContext, StoreAuthenticationActivity.class);
+    appContext.startActivity(intent);
+  }
+}
