157cf27d6dc8b8377aa52cc7492971a47895f29a
liubryann <liubryan.developer@gmail.com>
2019-11-26 19:34:42 +0000

    Bryan Liu: AddAccountButtonController, adds a new account for the selected user

---
+++ SalesApplication/app/src/main/java/com/b07/store/employee/AddAccountButton.java
@@ -0,0 +1,56 @@
+package com.b07.store.employee;
+
+import android.content.Context;
+import android.provider.ContactsContract.Data;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.b07.R;
+import com.b07.database.DatabaseInsertHelper;
+import com.b07.database.DatabaseSelectHelper;
+import com.b07.users.User;
+import com.b07.validation.Validator;
+import java.util.List;
+
+public class AddAccountButton implements View.OnClickListener {
+
+  private Context appContext;
+  private List<Integer> customers;
+
+  public AddAccountButton(Context context, List<Integer> customers) {
+    appContext = context;
+    this.customers = customers;
+  }
+
+  @Override
+  public void onClick(View v) {
+    TextView userId = ((AddNewAccountActivity) appContext)
+        .findViewById(R.id.employee_add_account_user_id);
+
+    TextView error = ((AddNewAccountActivity) appContext).findViewById(R.id.employee_add_account_error);
+
+    Integer parsedUserId = -1;
+    if (!Validator.validateEmpty(userId.getText().toString().trim())) {
+      parsedUserId = Integer.parseInt(userId.getText().toString().trim());
+    }
+
+    if (!Validator.validateUserId(parsedUserId, appContext) || !customers.contains(parsedUserId)) {
+      error.setText(R.string.user_id_error);
+    } else {
+
+      DatabaseSelectHelper.getUserAccounts(parsedUserId, appContext);
+
+      int accId = DatabaseInsertHelper.insertAccount(parsedUserId, true, appContext);
+      if (accId == -1){
+        error.setText(R.string.account_creation_error);
+      }
+      else {
+        Toast toast = Toast.makeText(appContext, "Creating account...", Toast.LENGTH_SHORT);
+        toast.show();
+        ((AddNewAccountActivity)appContext).finish();
+      }
+
+    }
+  }
+}
--- SalesApplication/app/src/main/java/com/b07/store/employee/AddNewAccountActivity.java
+++ SalesApplication/app/src/main/java/com/b07/store/employee/AddNewAccountActivity.java
@@ -1,8 +1,15 @@
 package com.b07.store.employee;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.b07.R;
+import com.b07.database.DatabaseSelectHelper;
+import com.b07.users.Roles;
+import com.b07.users.User;
+import java.util.List;
 
 public class AddNewAccountActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class AddNewAccountActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.add_account);
+
+    TextView customerView = findViewById(R.id.employee_add_account_users);
+    EditText userIdInput = findViewById(R.id.employee_add_account_user_id);
+    userIdInput.setTransformationMethod(null);
+
+    int roleId = DatabaseSelectHelper.getRoleIdFromName(Roles.CUSTOMER.name(), this);
+    List<Integer> customers = DatabaseSelectHelper.getUsersByRole(roleId, this);
+
+    String customerList = "";
+    User user;
+    for (int userId : customers) {
+      user = DatabaseSelectHelper.getUserDetails(userId, this);
+      customerList += user.getId() + " - " + user.getName() + "\n";
+    }
+
+    Button addAccountButton = findViewById(R.id.employee_add_account_button);
+    addAccountButton.setOnClickListener(new AddAccountButton(this, customers));
+    customerView.setText(customerList);
   }
 }
